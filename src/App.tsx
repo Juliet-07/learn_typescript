028fdfa825e36860ec67fe03f8e7e8dfe346a63d
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2023-03-19 14:32:18 +0100

    todolist completed in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+import { Todo } from "./model";
+// let name: string;
+// let age: number | string;
+// let isStudent: boolean;
+// let hobbies: string[];
+// let role: [number, string]; //tuples
+// type Person = {
+//   name: string;
+//   age?: number;
+// };
 
-let name: string;
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string]; //tuples
-type Person = {
-  name: string;
-  age?: number;
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
+  );
 };
-function App() {
-  return <div className="App">Hello World</div>;
-}
 
 export default App;
